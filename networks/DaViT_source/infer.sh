56af091ce1ac5ef9489a943c8a53be5ea29cd3ed
ThanhHung2610 <hungls1234506@gmail.com>
2023-07-15 19:35:24 +0700

    add davit source folder

---
+++ networks/DaViT_source/infer.sh
@@ -0,0 +1,7 @@
+python inference.py ../data/val/images/test \
+--output_dir ../output \
+--model DaViT_tiny \
+--batch-size 1 \
+--checkpoint ../pretrained/classification/DaViT_Pretrained.pth.tar
+
+python inference.py ../data/val/images/test --output_dir ../output --model DaViT_tiny --batch-size 1 --checkpoint ../pretrained/classification/DaViT_Pretrained.pth.tar
